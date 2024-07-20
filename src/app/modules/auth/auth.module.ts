8094dcace14a3ba6875d067bdbb094cd70922e92
Enrique García <enrique.garcia@redpontis.com>
2024-07-20 10:21:31 -0600

    refactor: 💡 Componente del login
    coloco el componente del login screen para que se pueda visualizar el
    login
    
    BREAKING CHANGE: 🧨 nuevo componete login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginScreenComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
