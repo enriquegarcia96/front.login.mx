6d1a11de7e179d85709a60dea13ee0c3a1b5882f
Enrique García <enrique.garcia@redpontis.com>
2024-07-21 00:28:02 -0600

    refactor: 💡 Agregando el modulo de login
    esta es la vista para mostrar el login y ya esta terminado la conexion
    hacia el backend

--- src/app/modules/auth/pages/login-screen/login-screen.component.ts
+++ src/app/modules/auth/pages/login-screen/login-screen.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../service/auth.service";
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-screen',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-screen.component.css']
 })
 export class LoginScreenComponent {
+  errorMessage: string = '';
 
+  formGroup: FormGroup = this.formBuilder.group({
+    email: ['correoejemplo@gmail.com', [Validators.required, Validators.email]],
+    password: ['pasarelaprueba1234', [Validators.required, Validators.minLength(6)]],
+  })
+
+
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {
+  }
+
+
+  login(): void {
+    const {email, password} = this.formGroup.value;
+
+    if (!email) {
+      this.errorMessage = 'Ingrese usuario';
+      return;
+    } else if (!password) {
+      this.errorMessage = 'Ingrese contraseña';
+      return;
+    }
+
+    this.authService.login(email, password).subscribe({
+      next: (token) => {
+        localStorage.setItem('tokenMx', token);
+        console.log('ENTRO AL TOKEN => ', token);
+
+        this.router.navigate(['home/dashboard']);
+        this.formGroup.reset();
+      },
+      error: error => {
+        console.log('Error de inicio de sesión:', error);
+        this.errorMessage = error;
+        this.formGroup.reset();
+      }
+    });
+  }
+
+  clearErrorMessage() {
+    this.errorMessage = '';
+  }
 }
